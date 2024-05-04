59862f9918222564d85db73240721cb84ed5a75c
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-05-04 14:10:56 +0200

    🧪 Benchmark current script

--- experiments/0001/golang/slow_test.go
+++ experiments/0001/golang/slow_test.go
@@ -1,11 +1,18 @@
 package golang
 
 import (
+	"fmt"
+	"os/exec"
 	"testing"
 )
 
 // Benchmark: 14.928s
 func BenchmarkSlow(b *testing.B) {
+	err := generateMutants()
+	if err != nil {
+		b.Fatal(err)
+	}
+
 	conf := Config{
 		MutantsDir:       "../gambit_out/mutants",
 		OriginalFilePath: "../src/Counter.sol",
@@ -15,4 +22,18 @@ func BenchmarkSlow(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Slow(conf)
 	}
+}
+
+func generateMutants() error {
+	cmd := exec.Command("gambit", "mutate", "--filename", "src/Counter.sol", "--contract", "Counter")
+	// cmd.Dir = "/Users/kamilchmielewski/Projects/sandbox_golang/experiments/0001"
+	cmd.Dir = "../"
+	println(cmd.Dir)
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Mutants generated")
+	return nil
 }
