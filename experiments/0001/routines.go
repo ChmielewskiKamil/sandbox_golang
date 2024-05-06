23bc448bea463127124304639404b03a1cd9c351
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-05-06 18:05:10 +0200

    👷 GTFO

--- experiments/0001/routines.go
+++ experiments/0001/routines.go
@@ -1,14 +1,5 @@
 package main
 
-import "sandbox_golang/experiments/0001/golang"
-
 func main() {
-	conf := golang.Config{
-		MutantsDir:       "./gambit_out/mutants",
-		OriginalFilePath: "src/Counter.sol",
-		OriginalFileName: "Counter.sol",
-		BackupFile:       "src/Backup.sol",
-	}
 
-	golang.Slow(conf)
 }
