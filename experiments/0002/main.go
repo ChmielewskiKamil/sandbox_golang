01ccd4a5eb9f92a1bfd9088fc21f7122c4c983cc
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-07-06 12:38:52 +0200

    👷 Binary tree comparison

--- experiments/0002/main.go
+++ experiments/0002/main.go
@@ -20,18 +20,37 @@ func Walk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
+	v1 := make(chan int)
+	v2 := make(chan int)
+
+	go func() {
+		Walk(t1, v1)
+		close(v1)
+	}()
+
+	go func() {
+		Walk(t2, v2)
+		close(v2)
+	}()
+
+	for {
+		x, ok1 := <-v1
+		y, ok2 := <-v2
+		if ok1 != ok2 || x != y {
+			return false
+		}
+
+		if !ok1 {
+			break
+		}
+	}
+
 	return true
 }
 
 func main() {
 	t1 := tree.New(1)
-	values := make(chan int)
-	go Walk(t1, values)
-	for {
-		select {
-		case v := <-values:
-			fmt.Println("Value:", v)
-		}
-	}
+	t2 := tree.New(1)
 
+	fmt.Println(Same(t1, t2))
 }
